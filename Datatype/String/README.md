e1d6b1b40d3bac5bf57527e8c8ca330d6f78e1c1
abid211051 <c211051@ugrad.iiuc.ac.bd>
2023-09-06 21:45:51 +0600

    string

--- Datatype/String/README.md
+++ Datatype/String/README.md
@@ -8,3 +8,18 @@ Example : x = "abidafnan"; z = x.slice(2, 7); z = "idafn";[2 to 7-1];</p>
 <p>For reverse derection:</p>
 <h4>slice(-start, -end)</h4>
 <p>Example : x = "abidafnan"; z = x.slice(-6, -2); z = "dafn";[(from end)6-1 to (from end)2];</p>
+
+<h3>includes("search string")</h3>
+<p>include return true if given string or substring is present in main string, else false</p>
+
+<h3>trim()</h3>
+<p>trim() remove all the white space before and end of the string</p>
+
+<h3>replace("search string", "replace string")</h3>
+<p>replace method change the given string with searched string, but it doesnot really change the main string. it just return new string with replaced value</p>
+
+<h3>indexof("search string")</h3>
+<p>indexof return first occurence position of the search string if found, else -1</p>
+
+<h3>split("separator")</h3>
+<p>split method return array of string by spliting the string in different part when ever get a separator</p>
--- Datatype/String/script.js
+++ Datatype/String/script.js
@@ -1,6 +1,30 @@
-let x = "abidafnan";
-let z = x.slice(2, 7);
-console.log(z, x);
+let slice_str = "abidafnan";
+let slice_res1 = slice_str.slice(1, 6);
+let slice_res2 = slice_str.slice(-6, -1);
+console.log(slice_res1, slice_res2, slice_str);
 
-let s = x.toUpperCase();
-console.log(s);
+let upp_str = "abcdefg";
+let upp_res = upp_str.toUpperCase();
+console.log(upp_res);
+console.log(upp_res.length);
+
+let rep_str = "hi how are you!";
+let rep_res1 = rep_str.replace("hi", "hlw");
+console.log(rep_res1);
+
+let trim_str = "   Learning js  ";
+let trim_res1 = trim_str.trim();
+console.log(trim_res1, trim_res1.length, trim_str.length);
+
+let incl_str = "is this string present?";
+console.log(incl_str.includes("in"))
+
+let index_str = "return the position of abid";
+let index_res1 = index_str.indexOf("po");
+console.log(index_res1);
+
+let spli_str = "hi return a array of, string";
+let spli_arr = spli_str.split(",");
+let spli_arr1 = spli_str.split(" ");
+console.log(spli_arr);
+console.log(spli_arr1);
